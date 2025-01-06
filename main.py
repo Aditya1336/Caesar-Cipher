065130c5606b51af2f628c07f2e51b9030554190
Aditya1336 <146954779+Aditya1336@users.noreply.github.com>
2025-01-06 20:27:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
-
-    for letter in original_text:
-        if encode_or_decode == "decode":
+    if encode_or_decode == "decode":
             shift_amount *= -1
+    for letter in original_text:
+        
         if not letter.isalpha():
             output_text+= letter
         else:
